58029d34514aefce47a2cd56f7835a2c86f7f470
giuli2803 <giulianopalombarini@gmail.com>
2024-09-11 22:54:43 -0300

    changes in ALU test bench, added  errors

--- simulation/alu_tb.v
+++ simulation/alu_tb.v
@@ -5,6 +5,14 @@ module alu_tb;
   // Parametros
   localparam NB_OP = 6;
   localparam NB_DATA = 8;
+  localparam ADD_OP = 6'b100000;
+  localparam SUB_OP = 6'b100010;
+  localparam AND_OP = 6'b100100;
+  localparam OR_OP  = 6'b100101;
+  localparam XOR_OP = 6'b100110;
+  localparam SRA_OP = 6'b000011;
+  localparam SRL_OP = 6'b000010;
+  localparam NOR_OP = 6'b100111;
 
   // Senales
   reg [NB_OP-1:0] i_op;
@@ -22,77 +30,80 @@ module alu_tb;
       .o_data(o_data)
   );
 
-  // Procedimiento para imprimir resultados del test
-  task print_result;
-    input [NB_OP-1:0] operation;
-    input signed [NB_DATA-1:0] expected;
-    input signed [NB_DATA-1:0] result;
-    begin
-      if (expected === result) begin
-        $display("Test Passed: OP = %b, Result = %d, Expected = %d", operation, result, expected);
-      end else begin
-        $display("Test Failed: OP = %b, Result = %d, Expected = %d", operation, result, expected);
-      end
-    end
-  endtask
-
   // Testbench
   initial begin
+
     // Test de suma
     i_data_A = 8'd10;
     i_data_B = 8'd5;
-    i_op = 6'b100000;  // ADD_OP
+    i_op = ADD_OP;
     #10;
-    print_result(i_op, i_data_A + i_data_B, o_data);
+    if(o_data !== (i_data_A + i_data_B)) begin
+      $error("Test Failed: OP = ADD_OP, Result = %d, Expected = %d", o_data, i_data_A + i_data_B);
+    end
 
     // Test de resta
     i_data_A = 8'd15;
     i_data_B = 8'd5;
-    i_op = 6'b100010;  // SUB_OP
+    i_op = SUB_OP;
     #10;
-    print_result(i_op, i_data_A - i_data_B, o_data);
+    if(o_data !== (i_data_A - i_data_B)) begin
+     $error("Test Failed: OP = SUB_OP, Result = %d, Expected = %d", o_data, i_data_A - i_data_B);
+    end
 
     // Test de AND
     i_data_A = 8'b11001100;
     i_data_B = 8'b10101010;
-    i_op = 6'b100100;  // AND_OP
+    i_op = AND_OP;
     #10;
-    print_result(i_op, i_data_A & i_data_B, o_data);
+    if(o_data !== (i_data_A & i_data_B)) begin
+      $error("Test Failed: OP = AND_OP, Result = %d, Expected = %d", o_data, i_data_A & i_data_B);
+    end
 
     // Test de OR
     i_data_A = 8'b11001100;
     i_data_B = 8'b10101010;
-    i_op = 6'b100101;  // OR_OP
+    i_op = OR_OP;
     #10;
-    print_result(i_op, i_data_A | i_data_B, o_data);
+    if(o_data !== (i_data_A | i_data_B)) begin
+      $error("Test Failed: OP = OR_OP, Result = %d, Expected = %d", o_data, i_data_A | i_data_B);
+    end
 
     // Test de XOR
     i_data_A = 8'b11001100;
     i_data_B = 8'b10101010;
-    i_op = 6'b100110;  // XOR_OP
+    i_op = XOR_OP;
     #10;
-    print_result(i_op, i_data_A ^ i_data_B, o_data);
+    if(o_data !== (i_data_A ^ i_data_B)) begin
+      $error("Test Failed: OP = XOR_OP, Result = %d, Expected = %d", o_data, i_data_A ^ i_data_B);
+    end
 
     // Test de Shift Right Arithmetic (SRA)
     i_data_A = -8'd16;  // -16 en complemento a dos
     i_data_B = 8'd2;
-    i_op = 6'b000011;  // SRA_OP
+    i_op = SRA_OP;
     #10;
-    print_result(i_op, i_data_A >>> i_data_B, o_data);
+    if(o_data !== (i_data_A >>> i_data_B)) begin
+      $error("Test Failed: OP = SRA_OP, Result = %d, Expected = %d",o_data, i_data_A >>> i_data_B);
+    end
 
     // Test de Shift Right Logical (SRL)
     i_data_A = 8'd16;
     i_data_B = 8'd2;
-    i_op = 6'b000010;  // SRL_OP
+    i_op = SRL_OP;
     #10;
-    print_result(i_op, i_data_A >> i_data_B, o_data);
+    if(o_data !== (i_data_A >> i_data_B)) begin
+      $error("Test Failed: OP = SRL_OP, Result = %d, Expected = %d", o_data, i_data_A >> i_data_B);
+    end
 
     // Test de NOR
     i_data_A = 8'b11001100;
     i_data_B = 8'b10101010;
-    i_op = 6'b100111;  // NOR_OP
+    i_op = NOR_OP;
     #10;
-    print_result(i_op, ~(i_data_A | i_data_B), o_data);
+    if(o_data !== (~(i_data_A | i_data_B))) begin
+      $error("Test Failed: OP = NOR_OP, Result = %d, Expected = %d",o_data,~(i_data_A | i_data_B));
+    end
 
     $finish;
   end
