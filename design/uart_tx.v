fe2c5f8241347d9e62d2db54e686e4eb3734c295
giuli2803 <giulianopalombarini@gmail.com>
2024-09-30 22:29:49 -0300

    UART_TX convertido a combinacional

--- design/uart_tx.v
+++ design/uart_tx.v
@@ -1,5 +1,6 @@
 module uart_tx #(
-    NB_DATA = 8
+    NB_DATA = 8,
+    NB_STOP = 1
 ) (
     input wire i_reset,
     input wire i_tick,
@@ -12,66 +13,76 @@ module uart_tx #(
 
   localparam IDLE_STATE = 1'b0;
   localparam SEND_STATE = 1'b1;
+  localparam FRAME_LEN = NB_DATA + 1 + (NB_STOP);
 
-  reg state;
-  reg [NB_DATA+2-1:0] data;
-  reg [3:0] tick_count;
+  reg state, next_state;
+  reg [3:0] tick_count, next_tick_count;
+  reg [3:0] data_count, next_data_count;
+  reg tx, next_tx;
+  reg [FRAME_LEN-1:0] data, next_data;  // Trama con bits de STOP y START
   reg tx_done;
-  reg tx;
-  integer index;
 
-  always @(posedge i_clk) begin
+  // State manager
+  always @(posedge i_clk, posedge i_reset) begin
     if (i_reset) begin
-      // reset
-      data <= {(NB_DATA + 2) {1'b0}};
-      tx <= 1;
-      tx_done <= 0;
-      tick_count <= 4'b0;
-      index <= 0;
       state <= IDLE_STATE;
-
+      tick_count <= 0;
+      data_count <= 0;
+      data <= {FRAME_LEN{1'b0}};
+      tx <= 1'b1;
     end else begin
-      case (state)
-        IDLE_STATE: begin
-          tx_done <= 0;
-          tx <= 1;
-          if (i_tx_start == 1'b1) begin
-            data  <= {1'b1, i_tx_data, 1'b0};
-            state <= SEND_STATE;
-          end
+      state <= next_state;
+      tick_count <= next_tick_count;
+      data_count <= next_data_count;
+      data <= next_data;
+      tx <= next_tx;
+    end
+  end
+
+  always @(*) begin
+    next_state = state;
+    tx_done = 1'b0;
+    next_tick_count = tick_count;
+    next_data_count = data_count;
+    next_data = data;
+    next_tx = tx;
+    case (state)
+      IDLE_STATE: begin
+        next_tx = 1'b1;
+        if (i_tx_start) begin
+          next_data = {{NB_STOP{1'b1}}, i_tx_data, 1'b0};
+          next_state = SEND_STATE;
+          next_data_count = 0;
+          next_tick_count = 0;
         end
+      end
 
-        SEND_STATE: begin
-          if (i_tick) begin
-
-            if (tick_count < 15) begin
-              tick_count <= tick_count + 1;
+      SEND_STATE: begin
+        next_tx = data[0];  // enviar siguiente bit
+        if (i_tick) begin
+          if (tick_count < 15) begin
+            next_tick_count = tick_count + 1;
+          end else begin
+            next_tick_count = 0;
+            next_data = data >> 1;  // En data[0] esta el siguiente bit
+            if (data_count < (FRAME_LEN - 1)) begin
+              next_state = IDLE_STATE;
+              tx_done = 1'b1;
             end else begin
-              // enviar siguiente bit
-              if (index < NB_DATA + 2) begin
-                tx    <= data[index];
-                index <= index + 1;
-              end else begin
-                // enviar ultimo dato
-                state   <= IDLE_STATE;
-                index   <= 0;
-                tx_done <= 1;
-              end
-
-              tick_count <= 4'b0;
+              next_data_count = data_count + 1;
             end
           end
-
         end
 
-        default: begin
-          state <= IDLE_STATE;
-          tx_done <= 0;
-          tick_count <= 4'b0;
-          index <= 0;
-        end
-      endcase
-    end
+      end
+
+      default: begin
+        state <= IDLE_STATE;
+        tx_done <= 0;
+        tick_count <= 4'b0;
+        index <= 0;
+      end
+    endcase
   end
 
   assign o_tx = tx;
