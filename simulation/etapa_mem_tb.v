d3bdc1b8fc5feb19a2e244ae5dae7d2ba42167fa
marcosraimondi1 <marcosraimondi1@hotmail.com>
2024-10-27 10:15:24 -0300

    formato y se agregan pruebas de signed en mem

--- simulation/etapa_mem_tb.v
+++ simulation/etapa_mem_tb.v
@@ -1,57 +1,58 @@
 `timescale 1ns / 1ps
 
 module etapa_mem_tb;
-    
-    integer j;// Contador de iteraciones
-    
-   //inputs
-    reg clk;
-    reg reset;
-    reg [4:0] register;
-    reg [31:0] data_to_write;
-    reg [31:0] Alu_result;
-    reg WB_write;
-    reg WB_mem_to_reg;
-    reg MEM_read;
-    reg MEM_write;
-    reg MEM_unsigned;
-    reg [1:0] MEM_byte_half_word;
-    
-    //outputs
-    wire out_WB_write;
-    wire out_WB_mem_to_reg;
-    wire [31:0] out_ALU_result;
-    wire [31:0] out_read_data;
-    wire [4:0]out_write_reg;
-  
-etapa_mem #() 
-etapa_mem_1 (
-    .i_clk(clk),
-    .i_reset(reset),
 
-     .i_write_reg(register),  // registro de destino donde se escriben los resultados en WB
-     .i_data_to_write_in_MEM(data_to_write),  // data a escribir en memoria
-     .i_ALU_result(Alu_result),
+  integer j;  // Contador de iteraciones
 
-    // senales de control (input)
-     .i_WB_write(WB_write),  // si 1 la instruccion escribe en el banco de registros
-     .i_WB_mem_to_reg(WB_mem_to_reg),  // si 0 guardo el valor de MEM (load) sino el valor de ALU (tipo R)
-     .i_MEM_read(MEM_read),  // si 1 leo la memoria de datos (LOAD)
-     .i_MEM_write(MEM_write),  // si 1 escribo en la memoria de datos (STORE)
-     .i_MEM_unsigned(MEM_unsigned),  // 1 unsigned 0 signed
-     .i_MEM_byte_half_word(MEM_byte_half_word),  // 00 byte, 01 half word, 11 word
+  //inputs
+  reg clk;
+  reg reset;
+  reg [4:0] register;
+  reg [31:0] data_to_write;
+  reg [31:0] Alu_result;
+  reg WB_write;
+  reg WB_mem_to_reg;
+  reg MEM_read;
+  reg MEM_write;
+  reg MEM_unsigned;
+  reg [1:0] MEM_byte_half_word;
 
-    // senales de control (output)
-     .o_WB_write(out_WB_write),  // si 1 la instruccion escribe en el banco de registros
-     .o_WB_mem_to_reg(out_WB_mem_to_reg),  // si 0 guardo el valor de MEM (load) sino el valor de ALU (tipo R)
+  //outputs
+  wire out_WB_write;
+  wire out_WB_mem_to_reg;
+  wire [31:0] out_ALU_result;
+  wire [31:0] out_read_data;
+  wire [4:0] out_write_reg;
 
-    // salidas de la etapa
-     .o_ALU_result(out_ALU_result),  // resultado de la ALU
-     .o_read_data(out_read_data),   // data leida de memoria
-     .o_write_reg(out_write_reg)    // registro de destino donde se escriben los resultados en WB
-);
+  etapa_mem #() etapa_mem_1 (
+      .i_clk  (clk),
+      .i_reset(reset),
 
-always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
+      .i_write_reg(register),  // registro de destino donde se escriben los resultados en WB
+      .i_data_to_write_in_MEM(data_to_write),  // data a escribir en memoria
+      .i_ALU_result(Alu_result),
+
+      // senales de control (input)
+      .i_WB_write(WB_write),  // si 1 la instruccion escribe en el banco de registros
+      .i_WB_mem_to_reg(WB_mem_to_reg),  // si 0 guardo el valor de MEM (load) sino el valor de ALU (
+      .i_MEM_read(MEM_read),  // si 1 leo la memoria de datos (LOAD)
+      .i_MEM_write(MEM_write),  // si 1 escribo en la memoria de datos (STORE)
+      .i_MEM_unsigned(MEM_unsigned),  // 1 unsigned 0 signed
+      .i_MEM_byte_half_word(MEM_byte_half_word),  // 00 byte, 01 half word, 11 word
+
+      // senales de control (output)
+      .o_WB_write(out_WB_write),  // si 1 la instruccion escribe en el banco de registros
+      .o_WB_mem_to_reg(out_WB_mem_to_reg),  // si 0 guardo el valor de MEM (load) sino el valor de A
+
+      // salidas de la etapa
+      .o_ALU_result(out_ALU_result),  // resultado de la ALU
+      .o_read_data (out_read_data),   // data leida de memoria
+      .o_write_reg (out_write_reg)    // registro de destino donde se escriben los resultados en WB
+  );
+
+  always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
+
+  reg [31:0] aux;
 
   // Testbench
   initial begin
@@ -68,7 +69,8 @@ always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
     MEM_write = 0;
     MEM_unsigned = 0;
     MEM_byte_half_word = 2'b11;
-    
+    aux = 0;
+
     //Aplico reseteo
     #20 reset = 1;
     #20 reset = 0;
@@ -76,32 +78,33 @@ always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
     // Caso 1: Escritura en memoria
     MEM_write = 1;
     MEM_read = 0;
-    MEM_byte_half_word = 2'b11; // Escritura de una palabra completa
-    
+    MEM_byte_half_word = 2'b11;  // Escritura de una palabra completa
+
     for (j = 0; j < 20; j = j + 1) begin
-        #20;  
-        data_to_write = data_to_write + 1; //  Sumo uno a la data
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
+      #20;
+      data_to_write = data_to_write + 1;  //  Sumo uno a la data
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
     end
-    
+
     #20;  // Esperar
- 
+
     // Caso 2: Lectura de memoria
-    Alu_result = {32{1'b0}}; // Empizo la l
+    Alu_result = {32{1'b0}};  // Empizo la l
     data_to_write = {32{1'b0}};
     MEM_write = 0;
     MEM_read = 1;
-    MEM_byte_half_word = 2'b11; // Escritura de una palabra completa
+    MEM_byte_half_word = 2'b11;  // Escritura de una palabra completa
     for (j = 0; j < 20; j = j + 1) begin
-        #20;  
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
-        if ((j)!= out_read_data) begin
-            $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", j, out_read_data);
+      #20;
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+      if ((j) != out_read_data) begin
+        $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", j,
+               out_read_data);
       end
-    end  
+    end
 
-     #20;
-     
+    #20;
+
     // ------------------------ Test con media palabra ---------------------------- //
     MEM_unsigned = 1;
     // Caso 1: Escritura en memoria
@@ -109,33 +112,34 @@ always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
     data_to_write = {32{1'b1}};
     MEM_write = 1;
     MEM_read = 0;
-    MEM_byte_half_word = 2'b01; // Escritura de una media palabra
-    
-    for (j = 0; j < 20; j = j + 1) begin
-        #20;  
-        data_to_write = data_to_write - 1; //  Sumo uno a la data
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
-    end
-    
-    #20;  // Esperar
- 
-    // Caso 2: Lectura de memoria
-    Alu_result = {32{1'b0}}; 
-    MEM_write = 0;
-    MEM_read = 1;     
-    data_to_write = {32{1'b1}};
-    MEM_byte_half_word = 2'b01; // Escritura de una media palabra
-    for (j = 0; j < 20; j = j + 1) begin
-        #20;
-        if (((32'h0000ffff)&(data_to_write - j))!= out_read_data) begin
-            $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", ((32'h0000ffff)&(data_to_write - j)), out_read_data);
-      end  
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
-        
-    end  
+    MEM_byte_half_word = 2'b01;  // Escritura de una media palabra
 
-     #20;
-     
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      data_to_write = data_to_write - 1;  //  Sumo uno a la data
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+    end
+
+    #20;  // Esperar
+
+    // Caso 2: Lectura de memoria
+    Alu_result = {32{1'b0}};
+    MEM_write = 0;
+    MEM_read = 1;
+    data_to_write = {32{1'b1}};
+    MEM_byte_half_word = 2'b01;  // Escritura de una media palabra
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      if (((32'h0000ffff) & (data_to_write - j)) != out_read_data) begin
+        $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.",
+               ((32'h0000ffff) & (data_to_write - j)), out_read_data);
+      end
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+
+    end
+
+    #20;
+
     // ------------------------ Test con un BYTE de palabra ---------------------------- //
     MEM_unsigned = 1;
     // Caso 1: Escritura en memoria
@@ -143,33 +147,111 @@ always #10 clk = ~clk;  // Reloj de 20ns -> 50MHz
     data_to_write = {32{1'b1}};
     MEM_write = 1;
     MEM_read = 0;
-    MEM_byte_half_word = 2'b00; // Escritura de un BYTE de palabra
-    
-    for (j = 0; j < 20; j = j + 1) begin
-        #20;  
-        data_to_write = data_to_write - 1; //  Sumo uno a la data
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
-    end
-    
-    #20;  // Esperar
- 
-    // Caso 2: Lectura de memoria
-    Alu_result = {32{1'b0}}; 
-    MEM_write = 0;
-    MEM_read = 1;     
-    data_to_write = {32{1'b1}};
-    MEM_byte_half_word = 2'b00; // Escritura de un BYTE de palabra
-    for (j = 0; j < 20; j = j + 1) begin
-        #20;
-        if (((32'h000000ff)&(data_to_write - j))!= out_read_data) begin
-            $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", ((32'h0000ffff)&(data_to_write - j)), out_read_data);
-      end  
-        Alu_result = Alu_result + 4; // Dirección de escritura (paso a la siguiente posición)
-        
-    end  
+    MEM_byte_half_word = 2'b00;  // Escritura de un BYTE de palabra
 
-     #20;
-              
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      data_to_write = data_to_write - 1;  //  Sumo uno a la data
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+    end
+
+    #20;  // Esperar
+
+    // Caso 2: Lectura de memoria
+    Alu_result = {32{1'b0}};
+    MEM_write = 0;
+    MEM_read = 1;
+    data_to_write = {32{1'b1}};
+    MEM_byte_half_word = 2'b00;  // Escritura de un BYTE de palabra
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      if (((32'h000000ff) & (data_to_write - j)) != out_read_data) begin
+        $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.",
+               ((32'h0000ffff) & (data_to_write - j)), out_read_data);
+      end
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+
+    end
+
+    #20;
+
+    // ------------------------ Test con lectura signed y escritura unsigned ---------------------------- //
+    $display("Test con lectura signed y escritura unsigned");
+    // 1: Escritura en memoria
+    MEM_unsigned = 1;
+    Alu_result = {32{1'b0}};
+    data_to_write = 32'h000000ff;  // byte negativo
+    MEM_write = 1;
+    MEM_read = 0;
+    MEM_byte_half_word = 2'b11;  // Escritura de WORD
+
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      data_to_write = data_to_write - 1;
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+    end
+
+    #20;  // Esperar
+    MEM_unsigned = 0;
+
+    // Caso 2: Lectura de memoria
+    Alu_result = {32{1'b0}};
+    MEM_write = 0;
+    MEM_read = 1;
+    MEM_byte_half_word = 2'b00;  // Lectura de un BYTE de palabra
+
+    aux = 32'hffffffff;
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      // se espera que se extienda el signo del dato a leer
+      if ((aux - j) != out_read_data) begin
+        $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", (aux - j),
+               out_read_data);
+      end
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+
+    end
+
+    #20;
+
+    // ------------------------ Test con lectura unsigned y escritura signed ---------------------------- //
+    $display("Test con lectura unsigned y escritura signed");
+    // 1: Escritura en memoria
+    MEM_unsigned = 0;
+    Alu_result = {32{1'b0}};
+    data_to_write = 32'h000000ff;  // byte negativo
+    MEM_write = 1;
+    MEM_read = 0;
+    MEM_byte_half_word = 2'b00;  // Escritura de BYTE
+
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      data_to_write = data_to_write - 1;
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+    end
+
+    #20;  // Esperar
+    MEM_unsigned = 1;
+
+    // Caso 2: Lectura de memoria
+    Alu_result = {32{1'b0}};
+    MEM_write = 0;
+    MEM_read = 1;
+    MEM_byte_half_word = 2'b11;  // Lectura de un WORD
+
+    aux = 32'hffffffff;
+    for (j = 0; j < 20; j = j + 1) begin
+      #20;
+      // se espera que se extienda el signo del dato cuando se escribio
+      if ((aux - j) != out_read_data) begin
+        $fatal("Failed Test Bench, expected = %d, out_read_data = %d ARE NOT EQUALS.", (aux - j),
+               out_read_data);
+      end
+      Alu_result = Alu_result + 4;  // Dirección de escritura (paso a la siguiente posición)
+
+    end
+
+    #20;
     $display("Passed ETAPA_MEM Test Bench");
 
     $finish;
--- simulation/etapa_wb_tb.v
+++ simulation/etapa_wb_tb.v
@@ -37,44 +37,47 @@ module etapa_wb_tb;
 
     // Caso 1: Escritura con datos de la ALU
     #10;
-    i_WB_write = 1;
-    i_WB_mem_to_reg = 1;      // Selección de datos de ALU
-    i_write_reg = 5'd15;      // Registro de destino
-    i_ALU_result = 32'hABCD1234;
+    i_WB_write      = 1;
+    i_WB_mem_to_reg = 1;  // Selección de datos de ALU
+    i_write_reg     = 5'd15;  // Registro de destino
+    i_ALU_result    = 32'hABCD1234;
 
     #10;
-    $display("Caso 1 - ALU Data: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data, o_write_reg, o_WB_write);
-    if((o_WB_data != i_ALU_result))begin
-        $fatal("Failed Test Bench, i_ALU_result(ALU) = %h, o_WB_data = %h", i_ALU_result, o_WB_data); 
+    $display("Caso 1 - ALU Data: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data,
+             o_write_reg, o_WB_write);
+    if ((o_WB_data != i_ALU_result)) begin
+      $fatal("Failed Test Bench, i_ALU_result(ALU) = %h, o_WB_data = %h", i_ALU_result, o_WB_data);
     end
-    if((o_write_reg != i_write_reg))begin
-        $fatal("Failed Test Bench, expected = %d, o_write_reg = %d", i_write_reg, o_write_reg); 
+    if ((o_write_reg != i_write_reg)) begin
+      $fatal("Failed Test Bench, expected = %d, o_write_reg = %d", i_write_reg, o_write_reg);
     end
-    if((o_WB_write != i_WB_write))begin
-        $fatal("Failed Test Bench, expected = %b, o_WB_write = %b", i_WB_write, o_WB_write); 
+    if ((o_WB_write != i_WB_write)) begin
+      $fatal("Failed Test Bench, expected = %b, o_WB_write = %b", i_WB_write, o_WB_write);
     end
     // Caso 2: Escritura con datos de memoria
     #10;
-    i_WB_mem_to_reg = 0;      // Selección de datos de memoria
-    i_read_data = 32'h1234ABCD;
+    i_WB_mem_to_reg = 0;  // Selección de datos de memoria
+    i_read_data     = 32'h1234ABCD;
 
     #10;
-    $display("Caso 2 - MEM Data: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data, o_write_reg, o_WB_write);
-    if((o_WB_data != i_read_data))begin
-        $fatal("Failed Test Bench, i_read_data(MEM) = %h, o_WB_data = %h", i_read_data, o_WB_data); 
+    $display("Caso 2 - MEM Data: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data,
+             o_write_reg, o_WB_write);
+    if ((o_WB_data != i_read_data)) begin
+      $fatal("Failed Test Bench, i_read_data(MEM) = %h, o_WB_data = %h", i_read_data, o_WB_data);
     end
     // Caso 3: Sin escritura habilitada
     #10;
-    i_WB_write = 0;           // Deshabilitar escritura
-    i_WB_mem_to_reg = 1;      // Selección de datos de ALU
+    i_WB_write      = 0;  // Deshabilitar escritura
+    i_WB_mem_to_reg = 1;  // Selección de datos de ALU
 
     #10;
-    $display("Caso 3 - No Write: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data, o_write_reg, o_WB_write);
-    if((o_WB_write != i_WB_write)| (o_WB_write != 0) )begin
-        $fatal("Failed Test Bench, expected = %b, o_WB_write = %b", i_WB_write, o_WB_write); 
+    $display("Caso 3 - No Write: o_WB_data = %h, o_write_reg = %d, o_WB_write = %b", o_WB_data,
+             o_write_reg, o_WB_write);
+    if ((o_WB_write != i_WB_write) | (o_WB_write != 0)) begin
+      $fatal("Failed Test Bench, expected = %b, o_WB_write = %b", i_WB_write, o_WB_write);
     end
-    
-     $display("Passed ETAPA_WB Test Bench");
+
+    $display("Passed ETAPA_WB Test Bench");
     // Fin de la simulación
     $finish;
   end
